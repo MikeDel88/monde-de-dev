6f89536f46ec9a54fc8a06616c91881a8fe177e4
Michael <delamarre.michael@gmail.com>
2026-08-24 10:26:06 +0200

    feat: ajout de JwtService.java (interface et implémentation)
    Permet de générer un token d'accès à l'api en utilisant le jwtEncoder.

---
+++ back/src/main/java/com/openclassrooms/mddapi/service/JwtServiceImpl.java
@@ -0,0 +1,40 @@
+package com.openclassrooms.mddapi.service;
+
+import com.openclassrooms.mddapi.model.User;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Log4j2
+@AllArgsConstructor
+@Service
+public class JwtServiceImpl implements JwtService {
+
+    private final JwtEncoder jwtEncoder;
+
+    @Override
+    public String generateAccessToken(User user) {
+        log.info("JWT Service : generateAccessToken");
+        Instant now = Instant.now();
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .subject(String.valueOf(user.getId()))
+                .issuedAt(now)
+                .expiresAt(now.plus(30, ChronoUnit.DAYS))
+                .build();
+
+        JwsHeader header = JwsHeader.with(SignatureAlgorithm.RS256).build();
+        String token = jwtEncoder.encode(JwtEncoderParameters.from(header, claims)).getTokenValue();
+
+        log.debug("JWT Service : Token de l'utilisateur {}", token);
+        return token;
+    }
+}
