2b202598d66bc1f7b82674bf716c91665d1f170f
Michael <delamarre.michael@gmail.com>
2026-08-25 11:59:38 +0200

    feature: clean du composant app.

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from "@angular/router";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    changeDetection: ChangeDetectionStrategy.Eager,
-    imports: [RouterOutlet]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet]
 })
 export class AppComponent {
-  title = 'mdd-client';
+
 }
