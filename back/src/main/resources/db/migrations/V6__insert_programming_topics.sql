85f0fb03aeac008f17fb00ddf0f4c0cc9f2b312b
Michael <delamarre.michael@gmail.com>
2026-08-25 17:53:09 +0200

    feature(migration)!: insertion de topics.

---
+++ back/src/main/resources/db/migrations/V6__insert_programming_topics.sql
@@ -0,0 +1,11 @@
+INSERT INTO topics (title, description) VALUES
+('Java', 'Langage de programmation orienté objet utilisé pour le développement d''applications d''entreprise.'),
+('JavaScript', 'Langage de script incontournable pour le développement web côté client et côté serveur.'),
+('Python', 'Langage polyvalent réputé pour sa simplicité, utilisé en data science, scripting et développement web.'),
+('Spring Boot', 'Framework Java facilitant la création d''applications backend robustes et prêtes pour la production.'),
+('Angular', 'Framework front-end développé par Google pour construire des applications web dynamiques.'),
+('React', 'Bibliothèque JavaScript pour créer des interfaces utilisateur réactives et composables.'),
+('DevOps', 'Pratiques et outils visant à rapprocher développement et exploitation pour livrer plus rapidement.'),
+('Bases de données', 'Conception, modélisation et optimisation des systèmes de stockage de données.'),
+('Architecture logicielle', 'Principes et patterns pour concevoir des applications maintenables et évolutives.'),
+('Tests & qualité de code', 'Bonnes pratiques de tests automatisés et d''assurance qualité du code.');
