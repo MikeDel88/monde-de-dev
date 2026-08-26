92c9228747c4fdaee30220312c55ff5c4750a478
Michael <delamarre.michael@gmail.com>
2026-08-26 16:04:35 +0200

    feature: ajout de la page topics qui contient la liste des thèmes et la possibilité de s'abonner dessus.

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Register } from './features/auth/pages/register/register';
 import {Login} from "./features/auth/pages/login/login";
 import {Feed} from "./features/feed/pages/feed";
 import {Home} from "./features/home/pages/home";
-import {TopicComponent} from "./features/topic/topic.component";
+import {TopicComponent} from "./features/topic/pages/topic.component";
 import {AuthGuard} from "./core/guards/auth-guard";
 import {GuestGuard} from "./core/guards/guest-guard";
 import {AuthLayout} from "./shared/layout/auth/auth-layout";
---
+++ front/src/app/features/topic/services/topic-service.ts
@@ -0,0 +1,17 @@
+import {inject, Service} from '@angular/core';
+import {HttpClient, httpResource, HttpResourceRef} from "@angular/common/http";
+import {Topic} from "../models/topic";
+import {Observable} from "rxjs";
+import {environment} from "../../../../environments/environment";
+
+@Service()
+export class TopicService {
+
+  private httpClient = inject(HttpClient);
+
+  topics: HttpResourceRef<Topic[] | undefined> = httpResource<Topic[]>(() => `${environment.apiUrl}/topics`);
+
+  subscribe(topicId: number): Observable<void> {
+    return this.httpClient.post<void>(`${environment.apiUrl}/topics/subscribe`, topicId);
+  }
+}
