83a9628e62becefff258074b4fd81f8d09e9e73b
Michael <delamarre.michael@gmail.com>
2026-08-26 14:18:34 +0200

    feature: ajout de la récupération de la liste des topics.
    Le Mapper défini sur l'utilisateur est abonné ou non au topic.

--- back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.openclassrooms.mddapi.model.Topic;
 import com.openclassrooms.mddapi.service.TopicService;
 
 @RestController
