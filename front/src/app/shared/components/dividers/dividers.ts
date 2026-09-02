b22c211915d063f0c3e3b05cd60ce2ed2204b1db
Michael <delamarre.michael@gmail.com>
2026-09-02 14:14:26 +0200

    refactor(divider): création du composant divider

---
+++ front/src/app/shared/components/dividers/dividers.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dividers',
+  imports: [],
+  templateUrl: './dividers.html',
+  styleUrl: './dividers.css',
+})
+export class Dividers {
+
+}
