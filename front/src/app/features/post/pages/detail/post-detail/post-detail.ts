4eb98d822d7b4bdce4ba0b7cf9202626be23c8b4
Michael <delamarre.michael@gmail.com>
2026-09-02 09:56:34 +0200

    feat(post): ajout de la mise en forme pour la page détail de l'article.

--- front/src/app/features/post/pages/detail/post-detail/post-detail.ts
+++ front/src/app/features/post/pages/detail/post-detail/post-detail.ts
@@ -1,5 +1,5 @@
 import {Component, inject} from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Post, PostService} from "../../../services/post-service";
 import {HttpResourceRef} from "@angular/common/http";
 import {DatePipe} from "@angular/common";
@@ -14,13 +14,20 @@ import {DatePipe} from "@angular/common";
 })
 export class PostDetail {
 
+  readonly titleComments: string = "Commentaires";
+
   private readonly activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+  private readonly router = inject(Router);
   readonly postId: string | null = this.activatedRoute.snapshot.params['id'];
   private readonly postService = inject(PostService);
   post: HttpResourceRef<Post | undefined> = this.postService.post;
 
   constructor() {
     this.postService.postId.set(this.postId);
+  }
+
+  onBack() {
+    this.router.navigate(['/feed']);
   }
 
 }
