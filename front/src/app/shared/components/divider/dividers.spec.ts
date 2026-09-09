a3c300cc23db65de8d79bf062312b8d839bdf9b1
Michael <delamarre.michael@gmail.com>
2026-09-09 10:51:43 +0200

    test(dividers): add unit test to verify presence of hr element in Dividers component

--- front/src/app/shared/components/divider/dividers.spec.ts
+++ front/src/app/shared/components/divider/dividers.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Dividers } from './dividers';
+import {describe,beforeEach, expect, it} from "@jest/globals";
+import {By} from "@angular/platform-browser";
 
 describe('Dividers', () => {
   let component: Dividers;
@@ -19,5 +21,10 @@ describe('Dividers', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should contain hr", () => {
+    const divider = fixture.debugElement.query(By.css('hr'));
+    expect(divider).toBeTruthy();
   });
 });
