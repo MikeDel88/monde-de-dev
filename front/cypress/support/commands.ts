eb82f685650399cea87820e32e7c66acb3e71e9a
Michael <delamarre.michael@gmail.com>
2026-09-10 17:43:13 +0200

    feat(commands): add registerUniqueUser and createPost commands with user fixture integration

--- front/cypress/support/commands.ts
+++ front/cypress/support/commands.ts
@@ -42,12 +42,20 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
+interface TestUser {
+  name: string
+  email: string
+  password: string
+}
+
 declare namespace Cypress {
     interface Chainable {
       getBySelector(selector: string): Chainable<JQuery>
       findBySelector(selector: string, search: string): Chainable<JQuery>
       login(emailOrName?: string, password?: string): Chainable<void>
       register(name?: string, email?: string, password?: string): Chainable<void>
+      registerUniqueUser(): Chainable<TestUser>
+      createPost(title: string, content: string): Chainable<void>
     }
 }
 
@@ -61,18 +69,43 @@ Cypress.Commands.add("findBySelector", (selector: string, search: string) => {
 
 Cypress.Commands.add('login', (emailOrName?: string, password?: string) => {
   cy.visit('/login')
-  cy.getBySelector('name').type(emailOrName ?? "test")
-  cy.getBySelector('password').type(password ?? "Test1234!")
+  cy.fixture("user").then((user) => {
+    cy.getBySelector('name').type(emailOrName ?? user.name)
+    cy.getBySelector('password').type(password ?? user.password)
+  })
+
   cy.getBySelector('btn-submit').click()
   cy.url().should('include', '/feed')
 })
 
 Cypress.Commands.add('register', (name?: string, email?: string, password?: string) => {
   cy.visit('/register')
-  cy.getBySelector('name').type(name ?? "test")
-  cy.getBySelector('email').type(email ?? "test@test.com")
-  cy.getBySelector('password').type(password ?? "Test1234!")
+  cy.fixture("user").then((user) => {
+    cy.getBySelector('name').type(name ?? user.name)
+    cy.getBySelector('email').type(email ?? user.email)
+    cy.getBySelector('password').type(password ?? user.password)
+  })
+
   cy.getBySelector('btn-submit').click()
+})
+
+Cypress.Commands.add('registerUniqueUser', () => {
+  const name = self.crypto.randomUUID()
+  const user: TestUser = { name: name.substring(0, 10), email: `${name}@test.com`, password: "Test1234!" }
+
+  cy.register(user.name, user.email, user.password)
+  cy.findBySelector("toast", "toast-success").should('exist')
+
+  return cy.wrap(user)
+})
+
+Cypress.Commands.add('createPost', (title: string, content: string) => {
+  cy.visit('/post')
+  cy.getBySelector('topic').select(1)
+  cy.findBySelector('title', 'input').type(title)
+  cy.getBySelector('content').type(content)
+  cy.getBySelector('btn-submit').find('button').should('not.be.disabled').click()
+  cy.url().should('include', '/feed')
 })
 
 
